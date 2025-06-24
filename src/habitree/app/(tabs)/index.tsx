e38df4627d3d3b25640a611ce968759af8aabf9e
Laurenz Born <s87171@htw-dresden.de>
2025-06-24 11:44:05 +0200

    Update Temporäre Fehlernachricht auskommentiert von nicht notwendigen Feature für Prototypvorstellung (index.tsx)

--- src/habitree/app/(tabs)/index.tsx
+++ src/habitree/app/(tabs)/index.tsx
@@ -149,7 +149,7 @@ export default function HomeScreen() {
         }
       );
     } catch (error) {
-      console.error('Fehler beim Aktualisieren des Status:', error);
+      //console.error('Fehler beim Aktualisieren des Status:', error);
       }
     };
 
