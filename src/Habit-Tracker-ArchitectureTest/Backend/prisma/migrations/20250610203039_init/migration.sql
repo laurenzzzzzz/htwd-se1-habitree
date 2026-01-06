00898ea5c0a083feae3f3e28f20af6fdb3e242d2
laurenzzzzzz <s87171@htw-dresden.de>
2026-01-06 08:07:49 +0100

    Delete alte habitree dummytests

--- src/Habit-Tracker-ArchitectureTest/Backend/prisma/migrations/20250610203039_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Habit" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    CONSTRAINT "Habit_pkey" PRIMARY KEY ("id")
-);
-
--- Insert initial test data
-INSERT INTO "Habit" ("name") VALUES ('Trinken 2L Wasser');
